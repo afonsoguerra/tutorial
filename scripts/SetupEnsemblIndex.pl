63b73a5ccc749c0a1ba367627069a2763bb7b135
Jose Afonso Guerra-Assuncao <a.guerra@ucl.ac.uk>
2019-10-16 14:40:45 +0100

    Adding setup scripts to help get things started (WIP) ; adding gitignore;

---
+++ scripts/SetupEnsemblIndex.pl
@@ -0,0 +1,25 @@
+
+#Check for latest versions
+
+wget --spider --no-remove-listing -q ftp://ftp.ensembl.org/pub/
+
+
+
+#Ask about version
+
+#Ask about species
+
+
+
+
+#Download file
+wget --spider -r --no-parent ftp://ftp.ensembl.org/pub/release-98/fasta/homo_sapiens/cdna/
+
+ftp://ftp.ensembl.org/pub/release-98/fasta/danio_rerio/cdna/
+
+
+
+#Setup kallisto index run and set it going
+
+
+
---
+++ scripts/SetupFirst.pl
@@ -0,0 +1,12 @@
+
+#Change usernames in the scripts if needed
+
+#Download Singularity Container if not there. 
+
+singularity pull shub://afonsoguerra/tutorial
+singularity exec tutorial.sif ls /
+
+
+#Ask about ensembl references that might be needed
+
+
