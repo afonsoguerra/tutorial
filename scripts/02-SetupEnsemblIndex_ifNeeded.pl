56b7f9dadd2bb227824217cfdff1dedaed8506ed
Jose Afonso Guerra-Assuncao <a.guerra@ucl.ac.uk>
2020-02-14 16:01:41 +0000

    testing ensembl R magic

--- scripts/02-SetupEnsemblIndex_ifNeeded.pl
+++ scripts/02-SetupEnsemblIndex_ifNeeded.pl
@@ -37,11 +37,8 @@ my $ensVer = &promptUser("What is the Ensembl version you want to download? ", $
 
 
 if($ensVer ne $latest){
-   #Regenerate Ensembl Archive list
-   system('curl \'https://www.ensembl.org/Help/ArchiveList\' | grep .archive.ensembl.org | sed -e \'s/li>/\n/g\' | grep cp-external | grep GRCh | egrep -o \'Ensembl ..: ... 20..\' | tr -d \':\' > .ensembl.archive.table')
-
-   #Parse Ensembl Archive list
-   open(IN, ".ensembl.archive.table") or die "Could not open input file: $!\n";
+   #Regenerate and Parse Ensembl Archive list
+   open(IN, 'curl \'https://www.ensembl.org/Help/ArchiveList\' | grep .archive.ensembl.org | sed -e \'s/li>/\n/g\' | grep cp-external | grep GRCh | egrep -o \'Ensembl ..: ... 20..\' | tr -d \':\' | ') or die "Could not retrieve archive list file: $!\n";
 
    my $archive = 'NA';
 
@@ -56,12 +53,16 @@ if($ensVer ne $latest){
    }
    close IN;
 
+   die "ERROR: Unfortunately I couldn't find the requested Ensembl version, please try again\n";
+
    #save matching ensembl host
 
-   die "$archive\n";
+   die "$archive .archive.ensembl.org\n";
+   #aug2017.archive.ensembl.org
 }
 else {
    #use/save defaults ensembl host
+   #www.ensembl.org
 
 }
 
@@ -69,6 +70,77 @@ else {
 #Ask about species
 
 my $ensSP = &promptUser("What is the Ensembl species you want to download?\nLikely choices are \"homo_sapiens\" or \"danio_rerio\"\nThe default is ", "homo_sapiens");
+
+#Save the current biomart dataset thingy here
+my @temp2 = split('_',$ensSP);
+my $datasetString = substr($temp2[0], 1,1).$temp2[1]."_gene_ensembl";
+
+die "$datasetString\n";
+
+
+
+### Setup/update R script to match dataset
+
+my $RScript = <<'RSCRIPT';
+
+#Processing Kallisto results
+#- read kallisto output
+#- gene level data 
+#- calculate cpm/tpm
+#- write matrix
+#- summary of read counts
+
+library(base)
+library(tximport)
+library(biomaRt)
+library(reshape)
+library(dplyr)
+
+accessions <- list.dirs(full.names=FALSE,recursive = FALSE)
+#accessions
+
+mart <- biomaRt::useMart(biomart="ensembl",dataset="'.$hsapiens_gene_ensembl.'", host="http://jan2019.archive.ensembl.org")
+
+t2g <- biomaRt::getBM(attributes = c("ensembl_transcript_id", "transcript_version", "ensembl_gene_id", "external_gene_name", "description", "transcript_biotype"), mart = mart)
+t2g$target_id <- paste(t2g$ensembl_transcript_id, t2g$transcript_version, sep=".") # append version number to the transcript ID
+t2g[,c("ensembl_transcript_id","transcript_version")] <- list(NULL) # delete the ensembl transcript ID and transcript version columns
+t2g <- dplyr::rename( t2g, gene_symbol = external_gene_name, full_name = description, biotype = transcript_biotype )
+t2g<-t2g[,c(ncol(t2g),1:(ncol(t2g)-1))]
+
+#Let's use tximport to summarize results into genes
+kallisto.dir<-paste0(accessions)
+kallisto.files<-file.path(kallisto.dir,"abundance.tsv")
+names(kallisto.files)<- accessions
+tx.kallisto <- tximport(kallisto.files, type = "kallisto", tx2gene = t2g, countsFromAbundance ="no")
+
+
+gb <- getBM(attributes=c("ensembl_gene_id","gene_biotype"), mart=mart)
+detach("package:biomaRt", unload=TRUE) #unload biomart because it creates problems with dplyr
+#gb_coding<-subset(gb, gb$gene_biotype=="protein_coding") #Select only protein coding genes
+genes<-gb$ensembl_gene_id
+counts<-as.data.frame(tx.kallisto$counts[row.names(tx.kallisto$counts) %in% genes, ])
+
+tpm <- as.data.frame(tx.kallisto$abundance[row.names(tx.kallisto$abundance) %in% genes, ])
+
+
+#Let's divide the count for the total read counts - we then split the count file and write a new file for each sample
+
+ids<-rownames(counts)
+
+total_counts<-apply(counts,2,sum)
+counts_divided<-sweep(counts, 2, total_counts, `/`)
+cpm<-counts_divided*1000000
+write.csv(round(total_counts),"total_counts_mapped.csv")
+
+#write results
+write.csv(counts, "rawcounts.csv")
+write.csv(cpm, "cpm.csv")
+write.csv(tpm, "tpm.csv")
+
+RSCRIPT
+
+
+
 
 
 print STDERR "Downloading Transcriptome file, please wait ...\n\n";
--- scripts/processing_rnaseq_step2.r
+++ scripts/processing_rnaseq_step2.r
@@ -5,62 +5,40 @@
 #- write matrix
 #- summary of read counts
 
-
 library(base)
-#library(BiocParallel)
-#library(RCurl)
 library(tximport)
-#library(readr)
 library(biomaRt)
 library(reshape)
 library(dplyr)
 
-
 accessions <- list.dirs(full.names=FALSE,recursive = FALSE)
-accessions
+#accessions
 
-#NOTE: KEEP TRACK OF THE RIGHT VERSION BELOW...
+mart <- biomaRt::useMart(biomart="ensembl",dataset="hsapiens_gene_ensembl", host="http://jan2019.archive.ensembl.org")
 
-mart <- biomaRt::useMart(biomart="ensembl",dataset="hsapiens_gene_ensembl", host="http://jan2019.archive.ensembl.org")#release95
-#mart <- biomaRt::useMart(biomart = "ensembl", dataset =  "hsapiens_gene_ensembl")
-#mart <- biomaRt::useMart(biomart = "ensembl", dataset = "drerio_gene_ensembl")
-#mart = useMart(biomart = "ENSEMBL_MART_ENSEMBL",dataset="hsapiens_gene_ensembl", host = "dec2016.archive.ensembl.org")
 t2g <- biomaRt::getBM(attributes = c("ensembl_transcript_id", "transcript_version", "ensembl_gene_id", "external_gene_name", "description", "transcript_biotype"), mart = mart)
 t2g$target_id <- paste(t2g$ensembl_transcript_id, t2g$transcript_version, sep=".") # append version number to the transcript ID
 t2g[,c("ensembl_transcript_id","transcript_version")] <- list(NULL) # delete the ensembl transcript ID and transcript version columns
 t2g <- dplyr::rename( t2g, gene_symbol = external_gene_name, full_name = description, biotype = transcript_biotype )
 t2g<-t2g[,c(ncol(t2g),1:(ncol(t2g)-1))]
+
 #Let's use tximport to summarize results into genes
 kallisto.dir<-paste0(accessions)
 kallisto.files<-file.path(kallisto.dir,"abundance.tsv")
 names(kallisto.files)<- accessions
 tx.kallisto <- tximport(kallisto.files, type = "kallisto", tx2gene = t2g, countsFromAbundance ="no")
 
-#Select only protein coding genes
-
-
-
-#load packages
-
-#library(sm)
-#install biomart
-#source("https://bioconductor.org/biocLite.R")
-#biocLite("biomaRt")
-#library(biomaRt)
-#mart <- biomaRt::useMart(biomart = "ensembl", dataset = "hsapiens_gene_ensembl")
 
 gb <- getBM(attributes=c("ensembl_gene_id","gene_biotype"), mart=mart)
 detach("package:biomaRt", unload=TRUE) #unload biomart because it creates problems with dplyr
-#gb_coding<-subset(gb, gb$gene_biotype=="protein_coding")
+#gb_coding<-subset(gb, gb$gene_biotype=="protein_coding") #Select only protein coding genes
 genes<-gb$ensembl_gene_id
 counts<-as.data.frame(tx.kallisto$counts[row.names(tx.kallisto$counts) %in% genes, ])
 
 tpm <- as.data.frame(tx.kallisto$abundance[row.names(tx.kallisto$abundance) %in% genes, ])
 
 
-
 #Let's divide the count for the total read counts - we then split the count file and write a new file for each sample
-#Get cpm
 
 ids<-rownames(counts)
 
@@ -69,15 +47,7 @@ counts_divided<-sweep(counts, 2, total_counts, `/`)
 cpm<-counts_divided*1000000
 write.csv(round(total_counts),"total_counts_mapped.csv")
 
-
 #write results
 write.csv(counts, "rawcounts.csv")
 write.csv(cpm, "cpm.csv")
 write.csv(tpm, "tpm.csv")
-
-
-
-
-
-
-
